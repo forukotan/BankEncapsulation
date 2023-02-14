cd6df609499704f3d2603bdcb804925fd52ef0ee
forukotan <forukotan1@gmail.com>
2023-02-14 17:35:03 -0600

    BANK ACCOUNT

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            var account = new BankAccount();
+            Console.WriteLine("How much are you depositing today?");
+            var amount = double.Parse(Console.ReadLine());
+
+
+            account.Deposit(amount);
+
+            Console.WriteLine($"your balance is {account.GetBalance()}");
+            
         }
     }
 }
